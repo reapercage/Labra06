ad60562829ec2e5eb064f1ac5250d28d8f171b26
reapercage <h4026@student.jamk.fi>
2017-01-28 19:55:25 +0200

    Tehtävä 3 asti tehtynä

---
+++ Labra06/CD.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMK.IT
+{
+    class Song
+    {
+        public string Name { get; set; }
+        public string Duration { get; set; }
+        public override string ToString()
+        {
+            return Name + " " + Duration;
+        }
+    }
+    class CD
+    {
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public int Year { get; set; }
+        private List<Song> songs;
+        public List<Song> Tracklist
+        {
+            get { return songs; }
+        }
+        public CD()
+        {
+            songs = new List<Song>();
+        }
+        public void AddTrack(Song sng)
+        {
+            songs.Add(sng);
+        }
+        public Song FindSong(int index)
+        {
+            if(index < songs.Count)
+            {
+                return songs.ElementAt(index);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
--- Labra06/Program.cs
+++ Labra06/Program.cs
@@ -10,7 +10,13 @@ namespace JAMK.IT.T1Henkilorekisteri
     {
         static void Main(string[] args)
         {
-            TestaaHenkiloRekisteri();
+            //Tehtävä1
+            //TestaaHenkiloRekisteri();
+            //Tehtävä2
+            //LevytJärjestykseen();
+            //Tehtävä3
+            KortitSekaisin();
+
         }
         static void TestaaHenkiloRekisteri()
         {
@@ -31,6 +37,67 @@ namespace JAMK.IT.T1Henkilorekisteri
             }
             //TODO kysy käyttäjältä  hetu ja haetaan sitä vastaava  henkilö näytölle
 
+        }
+        static void LevytJärjestykseen()
+        {
+            CD levy = new CD();
+            levy.Title = "In Times";
+            levy.Artist = "Enslaved";
+            levy.Year = 2015;
+            Song sng = new Song { Name = "Thurisaz Dreaming", Duration = "8:14" };
+            Song sng1 = new Song { Name = "Building with Fire", Duration = "8:50" };
+            Song sng2 = new Song { Name = "One Thousand Years of Rain", Duration = "8:14" };
+            Song sng3 = new Song { Name = "Nauthir Bleeding", Duration = "8:11" };
+            Song sng4 = new Song { Name = "In Times", Duration = "10:45" };
+            Song sng5 = new Song { Name = "Daylight", Duration = "8:56" };
+
+            levy.AddTrack(sng);
+            levy.AddTrack(sng1);
+            levy.AddTrack(sng2);
+            levy.AddTrack(sng3);
+            levy.AddTrack(sng4);
+            levy.AddTrack(sng5);
+            
+            foreach (Song s in levy.Tracklist)
+            {
+                Console.WriteLine("Kappale {0}", s.ToString());
+            }
+
+            Console.ReadKey();
+        }
+        static void KortitSekaisin()
+        {
+            string[] korttipakka = new string[52];
+            int x;
+            for(int i = 0; i < 52; i++)
+            {
+                if(i < 13)
+                {
+                    x = i + 1;
+                    korttipakka[i] = "Pata #" + x;
+                }
+                if(i > 12 && i < 26)
+                {
+                    x = i - 12;
+                    korttipakka[i] = "Hertta #" + x;
+                }
+                if (i > 25 && i < 39)
+                {
+                    x = i - 25;
+                    korttipakka[i] = "Risti #" + x;
+                }
+                if (i > 38 && i < 52)
+                {
+                    x = i - 38;
+                    korttipakka[i] = "Ruutu #" + x;
+                }
+            }
+            for (int i = 0; i < 52; i++)
+            {
+                x = i + 1;
+                Console.WriteLine(x + " kortti on " + korttipakka[i]);
+            }
+            Console.ReadKey();
         }
     }
 }
